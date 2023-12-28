5dfe788a4463c968ae8558934c9cc7c7b80a0f39
Israel Adewuyi <isistickz@gmail.com>
2023-12-28 16:27:52 +0300

    feat: :construction: Upd.

--- unzip.py
+++ unzip.py
@@ -5,5 +5,6 @@ with ZipFile("digit-recognizer.zip", 'r') as unzip:
     unzip.printdir() # <<-- Check inside zip file
 
     unzip.extract('train.csv') # <<-- Extract a single file
+    unzip.extract('test.csv')
 
     #unzip.extractall() # <<-- Extract all files at once
